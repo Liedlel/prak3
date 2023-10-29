e4d3a7e31148687538c483f268c2785dd256d05b
Liedlel <nursalilliedle@gmail.com>
2023-10-29 22:28:04 +0700

    first commit

---
+++ Main.java
@@ -0,0 +1,52 @@
+import java.util.Scanner;
+
+/**
+ * class limas berfungsi untuk menghitung volume limas
+ */
+class Limas {
+    // Atribut
+    private double alasLuas;
+    private double tinggi;
+    private double volume;
+
+    /**
+     * constructor limas
+     * @param alasLuas Luas alas limas
+     * @param tinggi tinggi limas
+     */
+    public Limas(double alasLuas, double tinggi) {
+        this.alasLuas = alasLuas;
+        this.tinggi = tinggi;
+    }
+
+    // Metode untuk menghitung volume
+    public void hitungVolume() {
+        this.volume = (1.0/3.0) * this.alasLuas * this.tinggi;
+    }
+
+    /**
+     * method untuk getvolume
+     * @return
+     */
+    public double getVolume() {
+        return this.volume;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Masukkan luas alas limas: ");
+        double alasLuas = scanner.nextDouble();
+
+        System.out.print("Masukkan tinggi limas: ");
+        double tinggi = scanner.nextDouble();
+
+        Limas limas = new Limas(alasLuas, tinggi);
+
+        limas.hitungVolume();
+
+        System.out.println("Volume limas adalah: " + limas.getVolume());
+    }
+}
